716ec7e30c7a06db9abbb7f98d509ac9851cbb7c
Ami Pandey <pandeyami915@gmail.com>
2026-06-08 18:10:41 +0530

    Implement Ransom Note solution in solution.cpp

---
+++ Week2/Day1/Ransom_Note/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mp;
+        for(char c:magazine){
+            mp[c]++;
+        }
+        for(char ch:ransomNote){
+            if(mp.find(ch)==mp.end()||mp[ch]==0){
+                return false;
+            }
+            mp[ch]--;
+        }
+        return true;
+        
+    }
+};
