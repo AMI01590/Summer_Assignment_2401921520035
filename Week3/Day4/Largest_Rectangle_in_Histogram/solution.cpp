1c6c60da79f35fd20a89eb45f6c04279ee61bd7b
Ami Pandey <pandeyami915@gmail.com>
2026-06-18 20:18:46 +0530

    Implement largestRectangleArea function in C++

---
+++ Week3/Day4/Largest_Rectangle_in_Histogram/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int maxA=0;
+        heights.push_back(0);
+        for(int i=0;i<heights.size();i++){
+            while(!st.empty() && heights[i]< heights[st.top()]){
+                int h=heights[st.top()];
+                st.pop();
+                int w;
+                if(st.empty()){
+                    w=i;
+                }
+                else{
+                    w=i-st.top()-1;
+                }
+                maxA=max(maxA,h*w);
+            }
+            st.push(i);
+
+        }
+        return maxA;
+        
+    }
+};
